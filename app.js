7967de02712cf9777563dc33e199685c920b3679
Ashutosh <anantashutosh10@gmail.com>
2024-06-04 17:55:34 +0530

    Added the Ejs file for various Pages

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const express = require('express');
 
+const uuid = require('uuid');
+
 const app = express();
 
 app.set('views', path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
+app.set('view engine','ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
@@ -16,7 +18,20 @@ app.get('/', function (req, res) {
 });
 
 app.get('/restaurants', function (req, res) {
-  res.render('restaurants');
+  const filePath = path.join(__dirname,'data','restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render('restaurants', {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+app.get('/restaurants/:id', function (req, res) {
+  const restaurantId = req.params.id;
+  res.render('restaurants-detail', { rid: restaurantId });
 });
 
 app.get('/recommend', function (req, res) {
@@ -25,7 +40,8 @@ app.get('/recommend', function (req, res) {
 
 app.post('/recommend', function (req, res) {
   const restaurant = req.body;
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+  restaurant.id = uuid.v4();
+  const filePath = path.join(__dirname,'data','restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
@@ -45,5 +61,4 @@ app.get('/about', function (req, res) {
   res.render('about');
 });
 
-
-app.listen(3000);       
+app.listen(3000);
